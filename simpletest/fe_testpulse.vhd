ba55b331d8c58653b0adb9272c5f5e2258675b4e
Thorsten Stezelberger <thorsten@lbl.gov>
2003-04-08 21:21:23 -0500

    nightly auto commit

--- simpletest/fe_testpulse.vhd
+++ simpletest/fe_testpulse.vhd
@@ -14,6 +14,7 @@ ENTITY fe_testpulse IS
 		RST			: IN STD_LOGIC;
 		-- enable flasher
 		enable		: IN STD_LOGIC;
+		divider		: IN STD_LOGIC_VECTOR(3 downto 0);
 		-- LED trigger
 		FE_TEST_PULSE	: OUT STD_LOGIC
 	);
@@ -25,8 +26,9 @@ ARCHITECTURE fe_test_pulsearch OF fe_testpulse IS
 BEGIN
 
 	PROCESS (RST,CLK)
-		VARIABLE cnt		: STD_LOGIC_VECTOR(13 downto 0);
-		VARIABLE cnt_old	: STD_LOGIC_VECTOR(13 downto 0);
+		VARIABLE cnt		: STD_LOGIC_VECTOR(15 downto 0);
+		VARIABLE cnt_o		: STD_LOGIC;
+		VARIABLE cnt_oo		: STD_LOGIC;
 		VARIABLE tick		: STD_LOGIC;
 		VARIABLE tick_old	: STD_LOGIC;
 		VARIABLE tick_old0	: STD_LOGIC;
@@ -34,15 +36,17 @@ BEGIN
 	BEGIN
 		IF RST='1' THEN
 			cnt			:= (others=>'0');
-			cnt_old		:= (others=>'0');
+			cnt_o		:= '0';
+			cnt_oo		:= '0';
 			tick		:= '0';
 			tick_old	:= '0';
 			tick_old0	:= '0';
 			tick_old1	:= '0';
 			FE_TEST_PULSE	<= '0';
 		ELSIF CLK'EVENT AND CLK='1' THEN
-			tick		:= cnt(8) XOR cnt_old(8);
-			cnt_old		:= cnt;
+			tick		:= cnt_o XOR cnt_oo;
+			cnt_oo		:= cnt_o;
+			cnt_o		:= cnt(CONV_INTEGER(divider)+8);
 			FE_TEST_PULSE	<= tick OR tick_old OR tick_old0 OR tick_old1;
 			tick_old1	:= tick_old0;
 			tick_old0	:= tick_old;
