baa8af8eda482d9f97fdbe0f849988d77c7b24c9
Arthur Jones <arthur@lbl.gov>
2003-07-07 16:42:51 -0500

    generate mif and .h file from a single source...

--- scripts/mkmif.sh
+++ scripts/mkmif.sh
@@ -5,7 +5,6 @@
 #
 sdir=../../scripts
 
-
 #
 # make a new build number...
 #
@@ -16,23 +15,7 @@ ${sdir}/newbld.sh
 #   fpga_type (from current dir fpga_type file...)
 #   build_num0 (from current dir build_num file...)
 #   build_num1 (from current dir build_num file...)
-#   versions:
-#      1: com_fifo
-#      2: com_dp
-#      3: daq (atwd, fadc)
-#      4: pulsers
-#      5: discriminator_rate
-#      6: local_coinc
-#      7: flasher_board
-#      8: trigger
-#      9: local_clock
-#     10: supernova
-#     11: unassigned
-#     12: unassigned
-#     13: unassigned
-#     14: unassigned
-#     15: unassigned
-#     16: unassigned
+#   versions (read from sdata.txt):
 #
 
 echo "-- mkmif.sh generated .mif file"
@@ -50,15 +33,14 @@ bn=`${sdir}/getbld.sh`
 
 echo "        1 :" `expr ${bn} % 65535`";"
 echo "        2 :" `expr ${bn} / 65535`";"
-echo "        3 :" `/bin/bash ${sdir}/getver.sh com_fifo.ver`";"
-echo "        4 :" `/bin/bash ${sdir}/getver.sh com_dp.ver`";"
-echo "        5 :" `/bin/bash ${sdir}/getver.sh daq.ver`";"
-echo "        6 :" `/bin/bash ${sdir}/getver.sh pulsers.ver`";"
-echo "        7 :" `/bin/bash ${sdir}/getver.sh discriminator_rate.ver`";"
-echo "        8 :" `/bin/bash ${sdir}/getver.sh local_coinc.ver`";"
-echo "        9 :" `/bin/bash ${sdir}/getver.sh flash_board.ver`";"
-echo "       10 :" `/bin/bash ${sdir}/getver.sh trigger.ver`";"
-echo "       11 :" `/bin/bash ${sdir}/getver.sh local_clock.ver`";"
-echo "       12 :" `/bin/bash ${sdir}/getver.sh supernova.ver`";"
-echo "[13..127] : 0;"
+vals=`cut -d ' ' -f 1 ${sdir}/sdata.txt`
+last=0
+for f in $vals; do
+    nm=`grep ^${f} ${sdir}/sdata.txt | cut -d ' ' -f 2`
+    printf "%9d : %d;\n" $f `${sdir}/getver.sh ${nm}.ver` 
+    last=${f}
+done
+
+last=`expr ${last} + 1`
+echo "[${last}..127] : 0;"
 
