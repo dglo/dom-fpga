c97362fd111c6ea51ceb7f32726403bb00767c0c
Arthur Jones <arthur@lbl.gov>
2003-07-08 07:54:24 -0500

    no longer need fpga_type file...

--- scripts/getfpga.sh
+++ scripts/getfpga.sh
@@ -1,11 +1,26 @@
 #!/bin/bash 
 
 #
-# get current fpga type
+# get current fpga type...
 #
-if [[ ! -f fpga_type ]] ;  then
-	echo "no fpga_type file..."
-	exit 1
+
+sdir=../../scripts
+
+#
+# get last two components of cwd...
+#
+wd1=`pwd | awk -v FS='/' '{ print $(NF-1) }'`
+wd2=`pwd | awk -v FS='/' '{ print $NF }' | sed 's/EPXA[14]//1'`
+
+if [[ ${#wd2} > 0 ]]; then
+    type=${wd1}_${wd2}
+else
+    type=${wd1}
 fi
-cat fpga_type
+
+if nm=`grep " ${type}\$" ${sdir}/tdata.txt`; then
+    echo $nm | cut -d ' ' -f 1
+else
+    echo "can't find fpga type for " ${type}
+fi
 
