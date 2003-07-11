a7c1d863edbc8c5d5ac050feee4f7a62879fc195
CVS-to-SVN conversion program <cvs2svn@icecube.wisc.edu>
2003-07-11 10:58:58 -0500

    This commit was manufactured by cvs2svn to create tag 'V01-01-01'.

--- localMods.mk
+++ localMods.mk
@@ -1,1 +1,4 @@
-BUILT_FILES += $(BUILD_DIR)/$(PUB_DIR_NAME)/dom-fpga/fpga-versions.h
+ifeq ("epxa10","$(strip $(PLATFORM))")
+  BUILT_FILES += $(BUILD_DIR)/$(PUB_DIR_NAME)/dom-fpga/fpga-versions.h
+endif
+
