436e2c1cf9678aac8814d9f22fe18e93fc6693ca
CVS-to-SVN conversion program <cvs2svn@icecube.wisc.edu>
2003-10-28 04:33:42 -0600

    This commit was manufactured by cvs2svn to create tag 'last_of_rev2'.

--- scripts/getver.sh
+++ scripts/getver.sh
@@ -9,15 +9,15 @@ if [[ ! -f ./${fname} ]] ; then
     if [[ ! -f ../common/${fname} ]] ; then
         if [[ ! -f ../../common/${fname} ]]; then
           echo 0 > ../../common/${fname}
-          cat ../../common/${fname}
+          cat ../../common/${fname} | tr -d '\r'
         else
-          cat ../../common/${fname}
+          cat ../../common/${fname} | tr -d '\r'
         fi
     else
-       cat ../common/${fname}
+       cat ../common/${fname} | tr -d '\r'
     fi
 else
-    cat ${fname}
+    cat ${fname} | tr -d '\r'
 fi
 
 
